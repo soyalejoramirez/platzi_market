84f364359129c7701b3f90389ea5c2eb80d3fd74
Alejandro Ramírez H <soyalejoramirez@gmail.com>
2020-07-21 13:02:53 -0500

    Clase 7. "Hola Mundo" con Spring Boot

---
+++ src/main/java/com/platzi/market/HolaMundoController.java
@@ -0,0 +1,15 @@
+package com.platzi.market;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String saludar() {
+        return "Nunca pares de aprender! 🚀";
+    }
+}
