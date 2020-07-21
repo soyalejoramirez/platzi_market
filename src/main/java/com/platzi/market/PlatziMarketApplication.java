dca4c88f03b929b526a71598e85bf6b141885666
Alejandro Ramírez H <soyalejoramirez@gmail.com>
2020-07-21 13:01:44 -0500

    Initial Commit

---
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzi/market/PlatziMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
