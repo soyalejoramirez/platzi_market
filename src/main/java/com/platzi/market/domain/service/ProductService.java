4171b261cb5c0abddcfda86543c8431455e99117
Alejandro Ramírez H <soyalejoramirez@gmail.com>
2020-07-23 12:37:09 -0500

    Clase 23 - Implementar la anotación @Service

---
+++ src/main/java/com/platzi/market/domain/service/ProductService.java
@@ -0,0 +1,38 @@
+package com.platzi.market.domain.service;
+
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll() {
+        return productRepository.getAll();
+    }
+
+    public Optional<Product> getProduct(int productId) {
+        return productRepository.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        return productRepository.getByCategory(categoryId);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public boolean delete(int productId) {
+        return getProduct(productId).map(product -> {
+            productRepository.delete(productId);
+            return true;
+        }).orElse(false);
+    }
+}
