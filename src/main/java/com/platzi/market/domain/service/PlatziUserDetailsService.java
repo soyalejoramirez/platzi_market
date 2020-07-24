71c478bdee89263d6fdd8e78ec7f37f9092a0575
Alejandro Ramírez H <soyalejoramirez@gmail.com>
2020-07-24 12:48:31 -0500

    Clase 32 - Configurar la seguridad de nuestra API con Spring Security

---
+++ src/main/java/com/platzi/market/domain/service/PlatziUserDetailsService.java
@@ -0,0 +1,17 @@
+package com.platzi.market.domain.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class PlatziUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("alejandro", "{noop}platzi", new ArrayList<>());
+    }
+}
---
+++ src/main/java/com/platzi/market/web/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.platzi.market.web.security;
+
+import com.platzi.market.domain.service.PlatziUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PlatziUserDetailsService platziUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(platziUserDetailsService);
+    }
+}
