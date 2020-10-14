26dd092e4003155e6962915ba19d7000f9106c15
Alejandro Ramírez <alejandroramirez@saimyr.com.co>
2020-10-14 15:45:51 -0500

    Se quita mapper innecesario del uses.

--- src/main/java/com/platzi/market/persistence/mapper/PurchaseItemMapper.java
+++ src/main/java/com/platzi/market/persistence/mapper/PurchaseItemMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
-@Mapper(componentModel = "spring", uses = {ProductMapper.class})
+@Mapper(componentModel = "spring")
 public interface PurchaseItemMapper {
 
     @Mappings({
