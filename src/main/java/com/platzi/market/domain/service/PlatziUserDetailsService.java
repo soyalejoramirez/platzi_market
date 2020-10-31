191bbbad9afd608776391b122ffc01315d4e0dc7
Alejandro Ramírez <alejandroramirez@saimyr.com.co>
2020-10-31 15:35:26 -0500

    Se corrige error que causaba que funcionara cualquier usuario.

--- src/main/java/com/platzi/market/domain/service/PlatziUserDetailsService.java
+++ src/main/java/com/platzi/market/domain/service/PlatziUserDetailsService.java
@@ -7,11 +7,25 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlatziUserDetailsService implements UserDetailsService {
+    private static List<User> users = new ArrayList();
+
+    public PlatziUserDetailsService() {
+        users.add(new User("alejandro", "{noop}platzi", new ArrayList<>()));
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User("alejandro", "{noop}platzi", new ArrayList<>());
+        Optional<User> user = users.stream().filter(u -> u.getUsername().equals(username)).findAny();
+
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException("User not found by name: " + username);
+        }
+
+        return user.get();
     }
 }
